b03695b61f9afd2d30d3b1126dac11c33e84d58c
David Marin <davidokun99@gmail.com>
2020-07-28 14:20:47 +0200

    Clean up project

---
+++ src/main/java/com/singletonapps/crm/ui/MainView.java
@@ -0,0 +1,12 @@
+package com.singletonapps.crm.ui;
+
+import com.vaadin.flow.component.orderedlayout.VerticalLayout;
+import com.vaadin.flow.router.Route;
+
+@Route("")
+public class MainView extends VerticalLayout {
+
+    public MainView() {
+
+    }
+}
